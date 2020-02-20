7d6d92113b161f8d3790bc7cd172bebc7687b61b
Noran Diaconu <ndiaconu@empoweredbenefits.com>
2020-02-20 09:33:21 -0500

    Initial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  styleUrls: ['app.component.css'],
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+  title: string;
+  constructor() {
+    this.title = 'Ultimate Angular';
+  }
+}

247cbdab8b81444ad082f1be732386022247f515
Noran Diaconu <ndiaconu@empoweredbenefits.com>
2020-02-21 10:11:07 -0500

    templates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  styleUrls: ['app.component.css'],
-  templateUrl: './app.component.html'
+  selector: "app-root",
+  styleUrls: ["app.component.css"],
+  templateUrl: "./app.component.html"
 })
 export class AppComponent {
   title: string;
+  numberOne = 1;
+  numberTwo = 2;
+  isHappy = true;
+  logo = "assets/img/logo.svg";
+  name = "Name";
+  handleClick() {
+    this.name = "Name";
+  }
+  handleClick2(value: string) {
+    this.name = value;
+  }
+  handleInput(event: any) {
+    this.name = event.target.value;
+  }
+  handleBlur(event: any) {
+    this.name = event.target.value;
+    console.log(event);
+  }
+  handleChange(value: string) {
+    this.name = value;
+  }
   constructor() {
-    this.title = 'Ultimate Angular';
+    this.title = "Ultimate Angular";
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { AppComponent } from "./app.component";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    FormsModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}

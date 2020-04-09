1a2d8ea993a4b3af91e3a941967c368e66bf7dd2
Noran Diaconu <ndiaconu@empoweredbenefits.com>
2020-04-09 14:27:41 -0400

    Completed component architecture

--- src/app/passenger-dashboard/passenger-dashboard.component.ts
+++ src/app/passenger-dashboard/passenger-dashboard.component.ts
@@ -27,4 +27,17 @@ export class PassengerDashboardComponent implements OnInit {
     }];
   }
 
+  handleEdit(event: Passenger) {
+    this.passengers = this.passengers.map((passenger: Passenger) => {
+      if (passenger.id === event.id) {
+        passenger = Object.assign({}, passenger, event);
+      }
+      return passenger;
+    });
+  }
+  handleRemove(event: Passenger) {
+    this.passengers = this.passengers.filter((passenger: Passenger) => {
+      return passenger.id !== event.id;
+    });
+  }
 }
--- src/app/passenger-dashboard/passenger-detail/passenger-detail.component.ts
+++ src/app/passenger-dashboard/passenger-detail/passenger-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit, OnChanges, Input, Output, EventEmitter } from "@angular/core";
 import { Passenger } from "../passenger";
 
 @Component({
@@ -6,13 +6,39 @@ import { Passenger } from "../passenger";
   templateUrl: "./passenger-detail.component.html",
   styleUrls: ["./passenger-detail.component.css"]
 })
-export class PassengerDetailComponent implements OnInit {
+export class PassengerDetailComponent implements OnInit, OnChanges {
   @Input()
   detail: Passenger;
 
-  constructor() { }
+  @Output()
+  remove: EventEmitter<Passenger> = new EventEmitter();
+  @Output()
+  edit: EventEmitter<Passenger> = new EventEmitter();
+
+  editing: boolean;
+
+  constructor() {
+    this.editing = false;
+  }
 
   ngOnInit() {
   }
+  ngOnChanges(changes) {
+    if (changes.detail) {
+      this.detail = Object.assign({}, changes.detail.currentValue);
+    }
+  }
 
+  onNameChange(value: string) {
+    this.detail.fullname = value;
+  }
+  toggleEdit() {
+    if (this.editing) {
+      this.edit.emit(this.detail);
+    }
+    this.editing = !this.editing;
+  }
+  onRemove() {
+    this.remove.emit(this.detail);
+  }
 }
